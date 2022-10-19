4402d89bd8e3b10de2199e582407c998b34cbbc8
unknown <jessehorner11@gmail.com>
2022-10-19 15:50:22 -0600

    added index.js code

--- achieve1/index.js
+++ achieve1/index.js
@@ -1,9 +1,22 @@
-const number = 4;
-console.log("Before", number);
+//Code not used
+// const number = 4;
+// console.log("Before", number);
 
-const squared = number ** 2;
-const remainder = number % Math.PI;
+// const squared = number ** 2;
+// const remainder = number % Math.PI;
 
-console.log("After - squared", squared);
-console.log("After - remainder", remainder);
-console.log("After - as money", `$${number.toFixed(2)}`);
+// console.log("After - squared", squared);
+// console.log("After - remainder", remainder);
+// console.log("After - as money", `$${number.toFixed(2)}`);
+
+//Code used for Achievement 1
+let wallet = 100;
+const walletAfter = wallet++;
+
+console.log(`wallet:${wallet}, wallet after purchase:${walletAfter}`);
+
+
+let account = 3;
+const accountAfter = ++account;
+
+console.log(`account:${account}, account after:${accountAfter}`);
